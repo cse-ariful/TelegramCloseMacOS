04e06c97137869873e4e7e27eda9a9c887475556
Ariful Jannat Arif <ariful.jannat@enosisbd.com>
2023-11-02 09:41:21 +0600

    Initial Commit

---
+++ TelegramClone/TelegramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TelegramCloneApp.swift
+//  TelegramClone
+//
+//  Created by Md. MA-Ariful Jannat on 2/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TelegramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
